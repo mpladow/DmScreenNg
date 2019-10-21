ba9fafa27f5079acc5f87dec384ad2b0335b0a0b
Michael La Dow <mpladow@gmail.com>
2019-10-21 23:45:08 +1100

    wip added manual section to dm screen

---
+++ src/app/home/screen/manual/manual.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Inject
+} from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { PageScrollService, PageScrollInstance } from "ngx-page-scroll-core";
+
+@Component({
+  selector: "app-manual",
+  templateUrl: "./manual.component.html",
+  styleUrls: ["./manual.component.scss"]
+})
+export class ManualComponent {
+  @ViewChild("container", { static: false }) private container: ElementRef;
+
+  constructor(
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: any
+  ) {}
+
+  public scrollToSection(el: HTMLElement) {
+    el.scrollIntoView();
+  }
+  // if the position of the top of a heading is less than the position of the top of container
+  // then set the active class to the heading of the next section heading.
+}
